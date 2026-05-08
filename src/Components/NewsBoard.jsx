b6d1ac69f73dcaab43602c16ec12450b8a92f12d
Harsh-Agarwal89 <harshagarwal8530@gmail.com>
2026-05-08 10:52:56 +0530

    Updated fix

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,22 +1,77 @@
+// import React, { useEffect, useState } from 'react'
+// import NewsItem from './NewsItem';
+
+// const NewsBoard = ({category}) => {
+
+//     const [articles,setArticles] = useState([]);
+
+//     useEffect(()=>{
+//         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+//         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+
+//     },[category])
+
+//   return (
+//     <div>
+//         <h2 className='text-center'> Latest <span className='badge bg-danger'>News</span></h2>
+//         {articles.map((news,index)=>{
+//             return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+//         })}
+//     </div>
+//   )
+// }
+
+// export default NewsBoard
+
 import React, { useEffect, useState } from 'react'
-import NewsItem from './NewsItem';
+import NewsItem from './NewsItem'
 
-const NewsBoard = ({category}) => {
+const NewsBoard = ({ category }) => {
 
-    const [articles,setArticles] = useState([]);
+  const [articles, setArticles] = useState([])
 
-    useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+  useEffect(() => {
 
-    },[category])
+    const fetchNews = async () => {
+      try {
+
+        const apiKey = import.meta.env.VITE_API_KEY
+
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+        console.log(data)
+
+        if (data.articles) {
+          setArticles(data.articles)
+        }
+
+      } catch (error) {
+        console.log("Error fetching news:", error)
+      }
+    }
+
+    fetchNews()
+
+  }, [category])
 
   return (
     <div>
-        <h2 className='text-center'> Latest <span className='badge bg-danger'>News</span></h2>
-        {articles.map((news,index)=>{
-            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-        })}
+      <h2 className='text-center'>
+        Latest <span className='badge bg-danger'>News</span>
+      </h2>
+
+      {articles.map((news, index) => (
+        <NewsItem
+          key={index}
+          title={news.title}
+          description={news.description}
+          src={news.urlToImage}
+          url={news.url}
+        />
+      ))}
     </div>
   )
 }
